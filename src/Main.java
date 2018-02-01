ac7e6b80635c69a30a8fa39e1e513361f0cd60d8
Michael Mann <mjam200@gmail.com>
2018-01-31 22:31:11 -0500

    makes a multicoloured quad

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,35 @@
-import org.lwjgl.glfw.GLFWVidMode;
-
+import org.lwjgl.opengl.GL;
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
 public class Main {
-    public Main(){}
-    public static void main(String[] args) {
+    public Main(){
         if (!glfwInit()){
-            throw new IllegalStateException("Failed to initialize glfw");
+            System.err.println("GLFW failed to initialize");
+            System.exit(1);
         }
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        long window = glfwCreateWindow(640,480,"Window", 0,0);
-        if (window==0){
-            throw new IllegalStateException("Failed to create window");
-        }
-        GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-        
-        glfwSetWindowPos(window, (vidMode.width()-640)/2, (vidMode.height()-480)/2);
-        
+        long window = glfwCreateWindow(650,480,"Window",0,0);
         glfwShowWindow(window);
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities();
         
         while (!glfwWindowShouldClose(window)){
             glfwPollEvents();
+            glClear(GL_COLOR_BUFFER_BIT);
+            glBegin(GL_QUADS);
+            glColor4f(1,0,1,0);
+            glVertex2f(-0.5f,0.5f);
+            glColor4f(0,1,0,0);
+            glVertex2f(0.5f,0.5f);
+            glColor4f(0,1,1,0);
+            glVertex2f(0.5f,-0.5f);
+            glColor4f(1,0,1,0);
+            glVertex2f(-0.5f,-0.5f);
+            glEnd();
+            glfwSwapBuffers(window);
         }
-        
         glfwTerminate();
+    }
+    public static void main(String[] args) {
+        new Main();
     }
 }
